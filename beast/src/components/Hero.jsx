1d3b05b86c1e8afef777e782d892d1d64c931189
Snehal Srivastava <118104081+SnehalSrivastava27@users.noreply.github.com>
2025-03-14 20:41:53 +0530

    Hero button added

--- beast/src/components/Hero.jsx
+++ beast/src/components/Hero.jsx
@@ -4,7 +4,12 @@ import './Hero.css';
 export const Hero=()=>{
     return(
         <div className="main">
+        <div className="bg-image"> 
             <img src={hero}></img>
+        </div>
+        <div className="hero-btn">
+        <button>TASTE THE DIFFERENCE</button>
+        </div>   
         </div>
     )
 }
